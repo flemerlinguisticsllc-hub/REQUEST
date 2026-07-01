d5083b43493b3fa0ee5a109ae1089df168bc74f3
flemerlinguisticsllc-hub <flemerlinguisticsllc@gmail.com>
2026-07-01 17:49:43 -0400

    Fix main.tsx to import App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App'
 import './index.css'
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-500 to-purple-600 flex items-center justify-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold text-white mb-4">REQUEST</h1>
-        <p className="text-xl text-blue-100">ASL Interpreter Request App</p>
-      </div>
-    </div>
-  )
-}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
